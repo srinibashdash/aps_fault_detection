8c6b418eacb348d1606a9f8f48c26580a032f048
srinibash Dash <srinibash.dash@gmail.com>
2022-11-29 22:51:50 +0530

    updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 HYPHEN_E_DOT= "-e ."
 
 
-def get_requirements()->list[str()]:
+def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list=requirement_file.readlines()
     requirement_list= [requirement_name.replace("\n", "") for requirement_name in requirement_list]
